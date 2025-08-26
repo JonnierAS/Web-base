96168e3aaaeb676b1d8d5abf308ce4779a1c7a2c
JonnierAS <jonniersalaverria@gmail.com>
2025-08-26 10:10:07 -0500

    refactor tabs for hidden button

--- front/src/shared/layout/components/TabsBar.jsx
+++ front/src/shared/layout/components/TabsBar.jsx
@@ -5,7 +5,9 @@ export const TabsBar = ({ tabs, activeTab, setActiveTab }) => {
         <button
           key={tab.key}
           onClick={() => setActiveTab(tab.key)}
-          className={`px-3 py-1 text-sm border-b-2 border-gray-400 cursor-pointer ${
+          className={`px-3 py-1 text-sm border-b-2 border-gray-400 cursor-pointer 
+          ${tab.label.length === 0 ? "hidden" : ""}
+          ${
             activeTab === tab.key
               ? "border-sky-500 text-sky-600 font-medium"
               : "border-transparent text-gray-600 hover:text-sky-500"
--- front/src/shared/layout/components/TabsBar.stories.jsx
+++ front/src/shared/layout/components/TabsBar.stories.jsx
@@ -40,26 +40,27 @@ export default {
     docs: {
       source: {
         code: `
-            export const TabsBar = ({ tabs, activeTab, setActiveTab }) => {
-            return (
-                <div className="flex border-b border-gray-200">
-                {tabs.map(tab => (
-                    <button
-                    key={tab.key}
-                    onClick={() => setActiveTab(tab.key)}
-                    className={\`
-                        px-3 py-1 text-sm border-b-2 border-gray-400 cursor-pointer 
-                        \${activeTab === tab.key
-                        ? "border-sky-500 text-sky-600 font-medium"
-                        : "border-transparent text-gray-600 hover:text-sky-500"
-                        }\`}
-                    >
-                    {tab.label}
-                    </button>
-                ))}
-                </div>
-            )}
-
+export const TabsBar = ({ tabs, activeTab, setActiveTab }) => {
+return (
+  <div className="flex border-b border-gray-200">
+    {tabs.map(tab => (
+      <button
+        key={tab.key}
+        onClick={() => setActiveTab(tab.key)}
+        className={\`px-3 py-1 text-sm border-b-2 border-gray-400 cursor-pointer 
+        \${tab.label.length === 0 ? "hidden" : ""}
+        \${
+          activeTab === tab.key
+            ? "border-sky-500 text-sky-600 font-medium"
+            : "border-transparent text-gray-600 hover:text-sky-500"
+        }\`}
+      >
+        {tab.label}
+      </button>
+    ))}
+  </div>
+)
+}
         `,
       },
     },
